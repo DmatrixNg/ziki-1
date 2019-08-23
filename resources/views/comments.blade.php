da923785bc7971cb5ecd752a75027c8426d22560
JeffOgah <jeff.ogah@gmail.com>
2019-08-23 13:09:36 +0100

    comments update

--- resources/views/comments.blade.php
+++ resources/views/comments.blade.php
@@ -2,13 +2,13 @@
 <div class="post-content">
     <img src="{{ $comment->image }}" class="img-fluid" style="border-radius:50%;object-fit:cover;" alt="user" width="55" height="56"/>
     <div class="post-content-body">
-        <p class="">
-            {{$comment->comment}}
-        </p>
-        <p class="">{{ $comment->username  }} - <small class="text-muted">@php
+    <p class="font-weight-bold m-0">{{ $comment->username  }} - <small class="text-muted">@php
            $created_at = $carbon->diffForHumans($comment->created_at);
            echo $created_at;
           @endphp</small></p>
+        <p class="m-0">
+            {{$comment->comment}}
+        </p>
     </div>
 </div>
 @empty
