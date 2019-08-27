806a58801a7d347ba00e459db4f6a3f5f33e7128
Motunrayo <motunrayo@softcom.ng>
2019-08-27 08:47:45 +0100

    reply made better

--- resources/views/comments.blade.php
+++ resources/views/comments.blade.php
@@ -5,7 +5,7 @@
 <ul style="padding: initial;">
 @forelse($comments as $comment)
 
-<div class="post-content mt-3 mb-2 pb-0">
+<div class="post-content mt-4 mb-2 pb-0">
     <img src="{{ $comment->image }}" class="img-fluid" style="border-radius:50%;object-fit:cover;" alt="user" width="55" height="56"/>
     <div class="post-content-body">
     <p class="font-weight-bold m-0">{{ '@'.$comment->username  }} - <small class="text-muted">@php
@@ -22,9 +22,22 @@
 
 @if($reply->parent_comment_id == $comment->id)
 
-<div class="ml-5 pl-4 " id="reply">
-    <img src="{{ $comment->image }}" class="img-fluid" style="border-radius:50%;object-fit:cover;" alt="user" width="40" height="45"/>
-    {{ $reply->comment }}
+<div class="ml-5 pl-4 d-flex align-items-center" id="reply">
+    <img src="{{ $comment->image }}" class="img-fluid mr-1" style="border-radius:50%;object-fit:cover;" alt="user" width="35" height="35"/>
+    <div>
+    <p class="font-weight-bold m-0" style="font-size: 13px;">
+        {{ '@'.$comment->username  }} - 
+        <small class="text-muted">@php
+        $created_at = $carbon->parse($comment->created_at);
+
+        echo $created_at->format('M jS, Y h:i A');
+        @endphp</small>
+    </p>
+    <p style="font-size: 13px;margin-bottom: 0;">
+        {{ $reply->comment }}
+    </p>
+    </div>
+    
 </div>
 
 
