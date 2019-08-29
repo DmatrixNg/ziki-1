510a374005e35cb770ad232125c9085d9095c74f
chibuikeIg <paulchibiukeigweze@gmail.com>
2019-08-29 14:20:38 -0700

    bug fix

--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -1,14 +1,19 @@
 let newtoolbarOptions = [
     ['bold', 'italic'],
-    ['blockquote'],
+    ['blockquote','code-block'],
     [{
       'list': 'ordered'
     }, {
       'list': 'bullet'
     }],
+    [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+    [{ 'indent': '-1'}, { 'indent': '+1' }],
     [{
       'header': [1, 2, 3, 4, 5, 6, false]
     }],
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'font': [] }],
+    [{ 'align': [] }],
     ['link', 'image'],
     ['clean']
   ];
